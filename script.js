96b270f57e644586c8e58d05bb3557227a58acbb
Vicent Botella Ferragud <vicentbotella.93@gmail.com>
2022-08-08 09:31:19 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * This function, called when the user clicks on the hamburger menu (displayed on devices whose screen is less than 800px wide), 
+ * toggles the CSS class "open", which adds and removes certain CSS features to the CSS classes "bar1", "bar2", "bar3" and 
+ * "mobile-menu" in order to display a suitable navigation menu for smartphone devices.
+ * @param {HTMLElement} menu - This is the reference to the "hamburger-icon" div. The CSS class "open" is given to (or remove from) 
+ * this element, which contains the elements that the CSS class "open" affects.
+ */
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
